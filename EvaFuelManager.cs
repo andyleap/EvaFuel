a3d723a2a5b452474128708af19ace371a58008f
Andy Leap <andyleap@gmail.com>
2014-09-21 12:29:35 -0400

    Initial Commit

---
+++ EvaFuelManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EvaFuel
+{
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, true)]
+    public class EvaFuelManager : MonoBehaviour
+    {
+        public void Awake()
+        {
+            GameEvents.onCrewOnEva.Add(this.onEvaStart);
+            GameEvents.onCrewBoardVessel.Add(this.onEvaEnd);
+        }
+
+        public void onEvaStart(GameEvents.FromToAction<Part, Part> data)
+        {
+            double fuel = data.from.RequestResource("MonoPropellant", 5);
+            if (fuel < 5)
+            {
+                data.to.RequestResource("EVA Propellant", 5 - fuel);
+            }
+        }
+
+        public void onEvaEnd(GameEvents.FromToAction<Part, Part> data)
+        {
+            double fuelleft = data.from.RequestResource("EVA Propellant", 5);
+            data.to.RequestResource("MonoPropellant", -fuelleft);
+        }
+    }
+}
